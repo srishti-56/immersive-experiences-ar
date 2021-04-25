e4344b05df5ffe7d0a6d1be7d373e7d2c51cbc80
Srishti <srishti-56@users.noreply.github.com>
2021-04-25 18:32:51 +0000

    fixed formatting

--- immersive-app/src/views/mood.vue
+++ immersive-app/src/views/mood.vue
@@ -2,15 +2,16 @@
   <div class="content-container">
     <div class="content-title-group not-found">
       <h2 class="title">View your Mood Board in AR</h2>
-      <p>
-        This page will display a default Mood Board.
-      </p>
+      <p>This page will display a default Mood Board.</p>
       <br />
       <h2 class="title">Resources</h2>
       <ul>
         <li>
-          <a href="https://srishti-56.medium.com/deploying-a-serverless-vue-js-app-using-github-codespaces-and-static-web-apps-5f5f51dabe76">
-            Deploying a Serverless Vue.js App using GitHub Codespaces and Static Web Apps
+          <a
+            href="https://srishti-56.medium.com/deploying-a-serverless-vue-js-app-using-github-codespaces-and-static-web-apps-5f5f51dabe76"
+          >
+            Deploying a Serverless Vue.js App using GitHub Codespaces and Static
+            Web Apps
           </a>
         </li>
       </ul>
